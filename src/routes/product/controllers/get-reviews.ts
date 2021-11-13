0d8f2a4b124cadc29093ed113485a276700d688d
raywu <rwu13795@gmail.com>
2021-11-13 15:19:19 -0500

    fixed refresh reviews bug

--- src/routes/product/controllers/get-reviews.ts
+++ src/routes/product/controllers/get-reviews.ts
@@ -12,11 +12,14 @@ interface Body {
   reviewPrimaryId: string;
   pageNum: number;
   filter?: string;
+  refresh?: boolean;
+  productId?: string;
 }
 
 export const getReviews = asyncWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { reviewPrimaryId, pageNum, filter } = req.body as Body;
+    const { reviewPrimaryId, pageNum, filter, refresh, productId } =
+      req.body as Body;
 
     const REVIEWS_PER_PAGE = 6;
     const skipNum = (pageNum - 1) * REVIEWS_PER_PAGE;
@@ -34,19 +37,29 @@ export const getReviews = asyncWrapper(
       if (!reviewDoc) return next(new Bad_Request_Error("No review found !"));
 
       return res.status(200).send({
-        message: "OK",
         reviews: reviewDoc.reviewsByRating[`${filter}`],
       });
+    }
+
+    if (refresh) {
+      const reviewDoc: ReviewDoc = await Review.findOne(
+        { productId },
+        { allReviews: { $slice: 6 } }
+      )
+        .select(["_id", "productId", "allRatings", "averageRating", "total"])
+        .lean();
+
+      if (!reviewDoc) return next(new Bad_Request_Error("No review found !"));
+
+      return res.status(200).send({ reviewDoc });
     }
 
     // $slice: [ <number to skip>, <number to return> ]
     const reviewDoc: ReviewDoc = await Review.findById(reviewPrimaryId, {
       allReviews: { $slice: [skipNum, returnNum] },
     }).lean();
-
     if (!reviewDoc) return next(new Bad_Request_Error("No review found !"));
-    return res
-      .status(200)
-      .send({ message: "OK", reviews: reviewDoc.allReviews });
+
+    return res.status(200).send({ reviews: reviewDoc.allReviews });
   }
 );
