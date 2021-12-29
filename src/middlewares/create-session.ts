96065b385e6aa963f3330c378b5d77fc2140d235
raywu <rwu13795@gmail.com>
2021-12-29 14:12:38 -0500

    fixing

--- src/middlewares/create-session.ts
+++ src/middlewares/create-session.ts
@@ -1,9 +1,16 @@
-import session from "express-session";
+import session, { CookieOptions } from "express-session";
 import MongoStore from "connect-mongo";
 import { config } from "dotenv";
+import { app } from "../app";
 
 if (process.env.NODE_ENV !== "production") {
   config();
+}
+
+let cookie: CookieOptions = { maxAge: 1000 * 60 * 60 };
+
+if (app.get("env") === "production") {
+  cookie.secure = true;
 }
 
 export const createSession = session({
@@ -13,15 +20,17 @@ export const createSession = session({
   // the MongoDBStore will set the expiration time the same as we set for the session
   // by using the expiration function offered by MongoDB
   proxy: true,
-  cookie: {
-    maxAge: 1000 * 60 * 60,
-    // httpOnly: true,
-    // secure: true,
-    // domain: "https://node-next-shop-client.herokuapp.com",
-    // sameSite: "none",
-  }, // 1 hour
+  cookie,
   // store: sessionStore, // additional config for using the MongoDBstore
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URI,
   }),
 });
+
+/**cookie: {
+    maxAge: 1000 * 60 * 60,
+    // httpOnly: true,
+    // secure: true,
+    // domain: "https://node-next-shop-client.herokuapp.com",
+    // sameSite: "none",
+  }, // 1 hour */
