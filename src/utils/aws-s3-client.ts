b5aeb9dffee41b06fa74674e026b75ed90efb06a
raywu <rwu13795@gmail.com>
2022-01-04 23:40:25 -0500

    clean some comments

--- src/utils/aws-s3-client.ts
+++ src/utils/aws-s3-client.ts
@@ -1,17 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
-// import aws from "aws-sdk";
-
-// aws.config.update({
-//   credentials: {
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID_MYAPP,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_MYAPP,
-//   },
-// });
 
 // you can find the region in the S3 management console
 const REGION = "us-east-2";
-
-// Create an Amazon S3 service client object.
 
 // The credential is in the ".env" file, the AWS sdk will find that credential
 // automatically while create the client
