80dc9a76bc4e1e74f5df4d4485ec6e16295d3dbc
raywu <rwu13795@gmail.com>
2022-01-07 14:25:38 -0500

    removed required attr in address_2 in user model

--- src/models/user/user-schema.ts
+++ src/models/user/user-schema.ts
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     address_1: { type: String, required: true },
-    address_2: { type: String, required: true },
+    address_2: { type: String },
     city: { type: String, required: true },
     state: { type: String, required: true },
     zip_code: { type: String, required: true },
