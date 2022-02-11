098f5b6ddb3c229a8b5ca3806a26647ddb0a91a7
raywu <rwu13795@gmail.com>
2022-02-11 16:04:06 -0500

    testing multiple signed cookies

--- src/routes/test-cloud-front.ts
+++ src/routes/test-cloud-front.ts
@@ -13,8 +13,8 @@ export const testCloudFront = asyncWrapper(
     //     expires: Math.floor((Date.now() + fiveMin) / 1000),
     //   });
 
-    const folder_1 = `${process.env.CLOUD_FRONT_URL}/testing/test-1/*`;
-    const folder_2 = `${process.env.CLOUD_FRONT_URL}/testing/test-2/*`;
+    const folder_1 = `${process.env.CLOUD_FRONT_URL}/users/*`;
+    const folder_2 = `${process.env.CLOUD_FRONT_URL}/groups/*`;
 
     const policy_1 = JSON.stringify({
       Statement: [
@@ -69,22 +69,22 @@ export const testCloudFront = asyncWrapper(
       domain: "node-next-shop-rw.store",
       httpOnly: true,
       // the corresponding path which the cookie let the user access
-      path: "/test-1",
+      path: "/users",
     });
     res.cookie("CloudFront-Signature", cookie_test1["CloudFront-Signature"], {
       domain: "node-next-shop-rw.store",
       httpOnly: true,
-      path: "/test-1",
+      path: "/users",
     });
     res.cookie("CloudFront-Policy", cookie_test2["CloudFront-Policy"], {
       domain: "node-next-shop-rw.store",
       httpOnly: true,
-      path: "/test-2",
+      path: "/groups",
     });
     res.cookie("CloudFront-Signature", cookie_test2["CloudFront-Signature"], {
       domain: "node-next-shop-rw.store",
       httpOnly: true,
-      path: "/test-2",
+      path: "/groups",
     });
 
     res.status(200).send("OK");
